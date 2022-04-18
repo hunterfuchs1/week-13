411f90172167a923e7c7b67cd96f793ecedafaaa
Hunter <jm0449zu@go.minnstate.edu>
2022-04-17 22:05:08 -0500

    VS 2022 Code

---
+++ Week11_G4_API/Controllers/CreditsController.cs
@@ -0,0 +1,123 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Week11_G4_API.Data;
+using Week11_G4_API.Models;
+
+namespace Week11_G4_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CreditsController : ControllerBase
+    {
+        private readonly SchoolofcollegesContext _context;
+
+        public CreditsController(SchoolofcollegesContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Credits
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Credit>>> GetCredits()
+        {
+            return await _context.Credits.ToListAsync();
+        }
+
+        // GET: api/Credits/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Credit>> GetCredit(string id)
+        {
+            var credit = await _context.Credits.FindAsync(id);
+
+            if (credit == null)
+            {
+                return NotFound();
+            }
+
+            return credit;
+        }
+
+        // PUT: api/Credits/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCredit(string id, Credit credit)
+        {
+            if (id != credit.CreditId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(credit).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CreditExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Credits
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Credit>> PostCredit(Credit credit)
+        {
+            _context.Credits.Add(credit);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (CreditExists(credit.CreditId))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetCredit", new { id = credit.CreditId }, credit);
+        }
+
+        // DELETE: api/Credits/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCredit(string id)
+        {
+            var credit = await _context.Credits.FindAsync(id);
+            if (credit == null)
+            {
+                return NotFound();
+
+            }
+            _context.Credits.Remove(credit);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool CreditExists(string id)
+        {
+            return _context.Credits.Any(e => e.CreditId == id);
+        }
+    }
+}
